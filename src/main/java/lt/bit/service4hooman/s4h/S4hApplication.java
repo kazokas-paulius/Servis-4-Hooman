314970d214c51d0ca65b5c251ac1a91913947c21
vygantastas <vygantastas@gmail.com>
2018-10-09 16:59:34 +0300

    Added maven dependences

---
+++ src/main/java/lt/bit/service4hooman/s4h/S4hApplication.java
@@ -0,0 +1,12 @@
+package lt.bit.service4hooman.s4h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4hApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4hApplication.class, args);
+	}
+}
